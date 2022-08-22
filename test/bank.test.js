d9ea3500b8d49ed3f17a0962da211f32bac5b561
delphine <delphine.mubiligi@gmail.com>
2022-08-22 23:36:14 +0100

    added more tests and formatting bank.test.js

--- test/bank.test.js
+++ test/bank.test.js
@@ -16,11 +16,11 @@ describe('Bank', () => {
   it('deposits and withdraws funds', () => {
     const account = new Bank;
 
-    account.deposit(100)
-    account.deposit(100)
-    account.withdraw(25)
+    account.deposit(100);
+    account.deposit(100);
+    account.withdraw(25);
 
-    expect(account.getBalance()).toEqual(175)
+    expect(account.getBalance()).toEqual(175);
   });
 
   it('deposits funds and prints a formatted bank statement', () => {
@@ -30,6 +30,29 @@ describe('Bank', () => {
       .mockImplementationOnce(() => Date.parse('22/08/2022'));
 
     account.deposit(100)
-    expect(account.printStatements()).toEqual('date || credit || debit || balance\n22/08/2022 || 100 ||   || 100')
-  })
+    expect(account.printStatements()).toEqual('date || credit || debit || balance\n22/08/2022 || 100 ||   || 100');
+  });
+
+  it('withdraws funds and prints a formatted bank statement', () => {
+    const account = new Bank;
+    jest
+      .spyOn(global.Date, 'now')
+      .mockImplementationOnce(() => Date.parse('22/08/2022'));
+
+    account.withdraw(100);
+    expect(account.printStatements()).toEqual('date || credit || debit || balance\n22/08/2022 ||   || 100 || -100');
+  });
+
+  it('deposits and withdraws funds, then prints a formatted bank statement', () => {
+    const account = new Bank;
+    jest
+      .spyOn(global.Date, 'now')
+      .mockImplementationOnce(() => Date.parse('22/08/2022'));
+
+    account.deposit(100);
+    account.deposit(100);
+    account.withdraw(25);
+    console.log(account.printStatements());
+    expect(account.printStatements()).toEqual('date || credit || debit || balance\n22/08/2022 || 100 ||   || 100\n22/08/2022 || 100 ||   || 200\n22/08/2022 ||   || 25 || 175');
+  });
 }); 
