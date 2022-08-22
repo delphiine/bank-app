e57d348e5eb73b1daced6de0f99babd24026a1a4
delphine <delphine.mubiligi@gmail.com>
2022-08-22 23:08:46 +0100

    prints a nicely formatted bank statement

--- bank.js
+++ bank.js
@@ -1,6 +1,17 @@
 class Bank {
   constructor () {
     this.balance = 0;
+    this.date = ''
+    this.bankStatements = []
+  }
+
+  _getDate () {
+    const date = new Date();
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+    const currentDate = day + '/' + month + '/' + year;
+    return this.date = currentDate;
   }
 
   getBalance () {
@@ -9,12 +20,41 @@ class Bank {
 
   deposit (amount) {
    this.balance += amount;
+   this._getDate()
+   const recipt = { 
+    date: this.date, 
+    credit: amount, 
+    debit: ' ',  
+    balance: this.balance
+  };
+   this.bankStatements.push(recipt);
   }
 
   withdraw (amount) {
     this.balance -= amount;
+    this._getDate()
+    const recipt = { 
+      date: this.date, 
+      credit: ' ', 
+      debit: amount,  
+      balance: this.balance
+    };
+    this.bankStatements.push(recipt);
   }
 
+  formatStatements () {
+    for (let i = 0; i < this.bankStatements.length ; i++) {
+      let statement = this.bankStatements[i];
+      this.recipt = (`${statement.date} || ${statement.credit} || ${statement.debit} || ${statement.balance}`)
+    }
+    const header = 'date || credit || debit || balance';
+   return `${header}\n${this.recipt}`
+  }
+
+  printStatements () {
+    console.log(this.formatStatements())
+    return this.formatStatements()
+  }
 }
 
 module.exports = Bank;
--- test/bank.test.js
+++ test/bank.test.js
@@ -22,4 +22,14 @@ describe('Bank', () => {
 
     expect(account.getBalance()).toEqual(175)
   });
+
+  it('deposits funds and prints a formatted bank statement', () => {
+    const account = new Bank;
+    jest
+      .spyOn(global.Date, 'now')
+      .mockImplementationOnce(() => Date.parse('22/08/2022'));
+
+    account.deposit(100)
+    expect(account.printStatements()).toEqual('date || credit || debit || balance\n22/08/2022 || 100 ||   || 100')
+  })
 }); 
