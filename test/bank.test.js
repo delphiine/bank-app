98e9b404beb366b5501ab7ea776b313f71a7b106
delphine <delphine.mubiligi@gmail.com>
2022-08-22 22:18:09 +0100

    testing adding and withdrawing funds

--- test/bank.test.js
+++ test/bank.test.js
@@ -3,13 +3,23 @@ const Bank = require('../bank')
 describe('Bank', () => {
   it('deposits funds into account', () => {
     const account = new Bank;
-    account.deposit(100)
-    expect(account.getBalance()).toEqual(100)
+    account.deposit(100);
+    expect(account.getBalance()).toEqual(100);
   });
 
   it('withdraw funds from account', () => {
     const account = new Bank;
-    account.withdraw(100)
-    expect(account.getBalance()).toEqual(-100)
+    account.withdraw(100);
+    expect(account.getBalance()).toEqual(-100);
+  });
+
+  it('deposits and withdraws funds', () => {
+    const account = new Bank;
+
+    account.deposit(100)
+    account.deposit(100)
+    account.withdraw(25)
+
+    expect(account.getBalance()).toEqual(175)
   });
 }); 
