9576c9686e433b9ae7a086b9cac69e89b5367430
delphine <delphine.mubiligi@gmail.com>
2022-08-26 13:55:20 +0100

    moved printStatements() to bank.js

--- bank.js
+++ bank.js
@@ -1,17 +1,9 @@
+const BankStatement = require('./BankStatement') 
+
 class Bank {
   constructor (balance=0) {
     this.balance = balance;
-    this.date = this._getDate();
-    this.bankStatements = [];
-  }
-
-  _getDate () {
-    const date = new Date();
-    const day = ('0' + date.getDate()).slice(-2);
-    const month = ('0' + (date.getMonth() + 1)).slice(-2);
-    const year = date.getFullYear();
-    const currentDate = day + '/' + month + '/' + year;
-    return currentDate;
+    this.statement = new BankStatement(this);
   }
 
   getBalance () {
@@ -20,42 +12,17 @@ class Bank {
 
   deposit (amount) {
     this.balance += amount;
-    this.date = this._getDate();
-    const receipt = {
-      date: this.date,
-      credit: amount,
-      debit: '',
-      balance: this.balance
-    };
-    this.bankStatements.push(receipt);
+    this.statement.formatDeposit(amount);
   }
 
   withdraw (amount) {
     this.balance -= amount;
-    this.date = this._getDate();
-    const receipt = {
-      date: this.date,
-      credit: '',
-      debit: amount,
-      balance: this.balance
-    };
-    this.bankStatements.push(receipt);
-  }
-
-  formatStatements () {
-    const receipt = [];
-    for (let i = 0; i < this.bankStatements.length; i++) {
-      const statement = this.bankStatements[i];
-      receipt.push(`${statement.date} || ${statement.credit} || ${statement.debit} || ${statement.balance}`);
-    }
-    receipt.reverse()
-    const header = 'date || credit || debit || balance';
-    return `${header}\n${receipt.join('\n')}`;
+    this.statement.formatWithdraw(amount);
   }
 
   printStatements () {
-    console.log(this.formatStatements());
-    return this.formatStatements();
+    console.log(this.statement.formatStatements());
+    return this.statement.formatStatements();
   }
 }
 
