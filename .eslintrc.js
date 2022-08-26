c5e290e079223f326fa8848714feb2775052aeb9
delphine <delphine.mubiligi@gmail.com>
2022-08-26 11:14:26 +0100

    configured the eslint config with jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,21 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
+    "jest/globals": true
   },
-  extends: 'standard',
+  extends: 'eslint:recommended',
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
   rules: {
-  }
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
+  },
+  "plugins": ["jest"]
 }
